603dd997a6c179bc8ca2e7ff0efda62a52660819
Kiewan Villatel <kiewan.villatel@gmail.com>
2019-04-06 19:48:31 +0200

    Fix typo

--- medium/longest_palindromic_substring.py
+++ medium/longest_palindromic_substring.py
@@ -13,13 +13,13 @@
 
 class Solution:
 
-    def _findBuildPalindrome(self, s, left, right):
+    def _findBestPalindrome(self, s, left, right):
         while left >= 0 and right < len(s) and s[left] == s[right]:
             left -= 1
             right += 1
         return s[left + 1:right]
 
-    def _get_longest_string(self, p1, p2):
+    def _getLongestString(self, p1, p2):
         return p1 if len(p1) > len(p2) else p2
 
     def longestPalindrome(self, s: str) -> str:
@@ -28,11 +28,11 @@ class Solution:
             if i + 1 < len(s) and s[i] == s[i + 1]:
                 left = i
                 rigth = i + 1
-                palindrome = self._findBuildPalindrome(s, left, rigth)
-                best_palindrome = self._get_longest_string(palindrome, best_palindrome)
-            if i > 0 and i < len(s) - 1:
-                palindrome = self._findBuildPalindrome(s, i - 1, i + 1)
-                best_palindrome = self._get_longest_string(palindrome, best_palindrome)
+                palindrome = self._findBestPalindrome(s, left, rigth)
+                best_palindrome = self._getLongestString(palindrome, best_palindrome)
+            if 0 < i < len(s) - 1:
+                palindrome = self._findBestPalindrome(s, i - 1, i + 1)
+                best_palindrome = self._getLongestString(palindrome, best_palindrome)
         return best_palindrome
 
 
